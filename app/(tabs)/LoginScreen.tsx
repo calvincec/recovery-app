7d16ba5f38e3dcb92c6745ed7b01326206037c09
Calvince Ndemo <calyndemo16@gmail.com>
2025-05-01 17:03:04 +0300

    removed the image bug

--- app/(tabs)/LoginScreen.tsx
+++ app/(tabs)/LoginScreen.tsx
@@ -37,7 +37,7 @@ export default function LoginScreen() {
       resizeMode="cover"
       style={styles.background}
     >
-		<img src="" alt="an image"  />
+		
       <StatusBar barStyle="light-content" />
 
       <View style={styles.overlay}>
